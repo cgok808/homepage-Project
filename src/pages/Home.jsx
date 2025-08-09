ede92ac9266238f125b56c79c28e57db12febc33
cgok808 <chaylin.gokan@gmail.com>
2025-08-09 13:36:02 -1000

    go away visual flicker

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,24 +9,28 @@ import WeatherDisplay from "../components/WeatherDisplay";
 const Home = () => {
   return (
     <Wallpaper>
-      <div className='flex flex-col justify-center items-center gap-8 md:flex-row md:justify-around min-h-screen p-4 box-border overflow-y-auto'>
-        {/* Left side - ToDoList (centered vertically on desktop, top on mobile) */}
-        <div className='flex justify-center md:items-center md:h-full mb-8 md:mb-0 min-w-0'>
+      <div className="flex flex-col justify-center items-center gap-8 md:flex-row md:justify-around min-h-screen p-4 box-border overflow-y-auto">
+        
+        {/* Left side - ToDoList */}
+        <div className="flex justify-center md:items-center md:h-full mb-8 md:mb-0 min-w-0">
           <ToDoList />
         </div>
 
-        {/* Right side - vertical stack with proper alignment */}
-        <div className='flex flex-col justify-center items-center min-w-0'>
-          <div className='lg:py-5 py-4 flex items-center justify-around gap-8'>
-            <WeatherDisplay className='md:w-auto w-48' />
-            <div className='md:w-auto w-48 flex justify-center'>
+        {/* Right side */}
+        <div className="flex flex-col justify-center items-center min-w-0 gap-[2rem] transform-gpu will-change-transform">
+          
+          {/* Weather + Time */}
+          <div className="lg:py-[1.25rem] py-4 flex items-center justify-around gap-8 transform-gpu will-change-transform">
+            <WeatherDisplay className="md:w-auto w-48" />
+            <div className="md:w-auto w-48 flex justify-center">
               <TimeDisplay />
             </div>
           </div>
-          <div>
-            <AppsDisplay />
-          </div>
+
+          {/* Apps */}
+          <AppsDisplay />
         </div>
+
       </div>
     </Wallpaper>
   );
